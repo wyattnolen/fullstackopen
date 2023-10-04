ef1a95c568055e3c4008e32022d509023242f834
wnolen <wyattnolen@gmail.com>
2023-10-03 21:06:33 -0400

    update

--- part1/1.6 - 1.11 -- unicafe/src/App.jsx
+++ part1/1.6 - 1.11 -- unicafe/src/App.jsx
@@ -1,31 +1,32 @@
-import { useState } from 'react'
-import Display from './components/Display'
-import Button from './components/Button'
+//https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps
+import { useState } from "react"
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
-  const increaseByOne = () => setCounter(counter + 1)
+  const [allClicks, setAll] = useState([])
 
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
 
-      <Button
-        handleClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        handleClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        handleClick={decreaseByOne}
-        text='minus'
-      />           
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
