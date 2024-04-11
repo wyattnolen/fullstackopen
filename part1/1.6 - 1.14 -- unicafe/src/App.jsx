7e186dfb51ee57cd0a62a264a9651c865cf8350d
wnolen <wyattnolen@gmail.com>
2024-04-10 20:16:50 -0400

    1.6

--- part1/1.6 - 1.14 -- unicafe/src/App.jsx
+++ part1/1.6 - 1.14 -- unicafe/src/App.jsx
@@ -1,37 +1,14 @@
-import { useState } from "react"
-import History from "./components/History"
-import Button from "./components/Button"
+import { useState } from 'react'
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
-
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right)
-  }
-
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-    setTotal(left + right)
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left'/>
-      <Button handleClick={handleRightClick} text='right'/>
-      {right}
-
-      <History allClicks={allClicks} />
-      <p>total {total}</p>
+      code here
     </div>
   )
 }
