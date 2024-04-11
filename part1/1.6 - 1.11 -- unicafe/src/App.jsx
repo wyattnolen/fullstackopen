5e5a92092c3af811b3a1d1349508548003807cf8
wnolen <wyattnolen@gmail.com>
2024-04-10 20:02:36 -0400

    course material

--- part1/1.6 - 1.11 -- unicafe/src/App.jsx
+++ part1/1.6 - 1.11 -- unicafe/src/App.jsx
@@ -1,32 +1,37 @@
-//https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps
 import { useState } from "react"
+import History from "./components/History"
+import Button from "./components/Button"
 
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
   }
 
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
     setRight(right + 1)
+    setTotal(left + right)
   }
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button handleClick={handleLeftClick} text='left'/>
+      <Button handleClick={handleRightClick} text='right'/>
       {right}
 
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
+      <p>total {total}</p>
     </div>
   )
 }
