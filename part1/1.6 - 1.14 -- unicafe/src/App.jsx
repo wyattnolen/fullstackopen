e5fcb097b44025abec9db462759d83ede9f8fc61
wnolen <wyattnolen@gmail.com>
2024-04-10 21:06:36 -0400

    1.6

--- part1/1.6 - 1.14 -- unicafe/src/App.jsx
+++ part1/1.6 - 1.14 -- unicafe/src/App.jsx
@@ -1,14 +1,34 @@
 import { useState } from 'react'
+import Button from './components/Button'
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => {
+    setGood(good + 1);
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1);
+  }
+
   return (
     <div>
-      code here
+      <h1>Unicafe Feedback</h1>
+      <h2>Give Feedback</h2>
+      <Button handleClick={handleGood} text="Good" />
+      <Button handleClick={handleNeutral} text="Neutral" />
+      <Button handleClick={handleBad} text="Bad" />
+      <h2>Statistics</h2>
+      Good: {good}
+      Neutral: {neutral}
+      Bad: {bad}
     </div>
   )
 }
