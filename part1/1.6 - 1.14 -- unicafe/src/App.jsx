66a21857ccf311dffd9264b1d32290f6615a4c90
wnolen <wyattnolen@gmail.com>
2024-04-18 18:45:18 -0400

    1.8

--- part1/1.6 - 1.14 -- unicafe/src/App.jsx
+++ part1/1.6 - 1.14 -- unicafe/src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
 import Button from './components/Button'
 
+const Statistics = ({good, neutral, bad}) => {
+  const sum = good + neutral + bad;
+  const average = (good * 1 + bad * -1) / sum;
+  const positive = (good / sum) * 100;
+
+  return (
+    <div>
+      Good: {good}
+      Neutral: {neutral}
+      Bad: {bad}
+      All: {sum}
+      Average: {average}
+      Positive: {positive}%
+    </div>
+  )
+
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   const handleGood = () => {
-  
     setGood(good + 1);
   }
 
@@ -19,24 +36,21 @@ const App = () => {
     setBad(bad + 1);
   }
 
-  const sum = good + neutral + bad;
-  const average = (good * 1 + bad * -1) / sum;
-  const positive = (good / sum) * 100;
+
 
   return (
     <div>
       <h1>Unicafe Feedback</h1>
-      <h2>Give Feedback</h2>
-      <Button handleClick={handleGood} text="Good" />
-      <Button handleClick={handleNeutral} text="Neutral" />
-      <Button handleClick={handleBad} text="Bad" />
-      <h2>Statistics</h2>
-      Good: {good}
-      Neutral: {neutral}
-      Bad: {bad}
-      All: {sum}
-      Average: {average}
-      Positive: {positive}%
+      <section>
+        <h2>Give Feedback</h2>
+        <Button handleClick={handleGood} text="Good" />
+        <Button handleClick={handleNeutral} text="Neutral" />
+        <Button handleClick={handleBad} text="Bad" />
+      </section>
+      <section>
+        <h2>Statistics</h2>
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      </section>
     </div>
   )
 }
