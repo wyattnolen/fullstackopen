9aae8fc495130115ad85b8c4be0abebb81f087d9
wnolen <wyattnolen@gmail.com>
2024-04-18 20:05:47 -0400

    1.13

--- part1/1.12 - 1.14 -- anecdotes/src/App.jsx
+++ part1/1.12 - 1.14 -- anecdotes/src/App.jsx
@@ -14,15 +14,23 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
   const handleAnecodteClick = () => {
-    const randomAnecdoteIndex = Math.floor(Math.random() * anecdotes.length);
-    setSelected(randomAnecdoteIndex);
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  }
+
+  const handleVoteClick = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += + 1
+    setVotes(newVotes);
   }
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>{votes[selected]}</p>
+      <Button handleClick={handleVoteClick} text="Vote" />
       <Button handleClick={handleAnecodteClick} text="Get Anecdote" />
     </div>
   )
