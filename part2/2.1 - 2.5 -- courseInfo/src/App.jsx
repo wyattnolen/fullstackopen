22fb06aff45d03a5c9020ec67e07119a4ff7690d
wnolen <wyattnolen@gmail.com>
2024-04-29 22:09:14 -0400

    2.1

--- part2/2.1 - 2.5 -- courseInfo/src/App.jsx
+++ part2/2.1 - 2.5 -- courseInfo/src/App.jsx
@@ -1,33 +1,29 @@
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
+import Course from './components/Course'
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
-    parts: [ 
+    parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
-  };
+  }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts}/>
-      <Total parts={course.parts} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
